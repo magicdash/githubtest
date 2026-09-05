34c3727f47d515e27ab1e297b40b3e40c9113a98
Momolulu <71570027+magicdash@users.noreply.github.com>
2026-09-05 14:53:04 -0400

    Fix nested OAuth popups in Android WebView

--- android-template/MainActivity.java
+++ android-template/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Message;
 import android.view.ViewGroup;
+import android.webkit.CookieManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
@@ -14,62 +15,80 @@ import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.BridgeWebChromeClient;
 
 public class MainActivity extends BridgeActivity {
+  private WebView mainWebView;
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    WebView mainWebView = getBridge().getWebView();
-    mainWebView.getSettings().setJavaScriptEnabled(true);
-    mainWebView.getSettings().setDomStorageEnabled(true);
-    mainWebView.getSettings().setSupportMultipleWindows(true);
-    mainWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+    mainWebView = getBridge().getWebView();
+    configureWebView(mainWebView);
 
     mainWebView.setWebChromeClient(new BridgeWebChromeClient(getBridge()) {
       @Override
       public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
-        final WebView popup = new WebView(MainActivity.this);
-        popup.setBackgroundColor(Color.WHITE);
-        popup.getSettings().setJavaScriptEnabled(true);
-        popup.getSettings().setDomStorageEnabled(true);
-        popup.getSettings().setSupportMultipleWindows(true);
-        popup.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-
-        popup.setWebViewClient(new WebViewClient() {
-          @Override
-          public boolean shouldOverrideUrlLoading(WebView v, WebResourceRequest request) {
-            return false;
-          }
-        });
-
-        popup.setWebChromeClient(new WebChromeClient() {
-          @Override
-          public void onCloseWindow(WebView window) {
-            ViewGroup parent = (ViewGroup) window.getParent();
-            if (parent != null) parent.removeView(window);
-            window.destroy();
-          }
-        });
-
-        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
-          FrameLayout.LayoutParams.MATCH_PARENT,
-          FrameLayout.LayoutParams.MATCH_PARENT
-        );
-        addContentView(popup, params);
-
-        WebView.WebViewTransport transport = (WebView.WebViewTransport) resultMsg.obj;
-        transport.setWebView(popup);
-        resultMsg.sendToTarget();
-        return true;
+        return createPopup(view, resultMsg);
       }
 
       @Override
       public void onCloseWindow(WebView window) {
-        if (window != mainWebView) {
-          ViewGroup parent = (ViewGroup) window.getParent();
-          if (parent != null) parent.removeView(window);
-          window.destroy();
-        }
+        closePopup(window);
       }
     });
+  }
+
+  private void configureWebView(WebView webView) {
+    webView.getSettings().setJavaScriptEnabled(true);
+    webView.getSettings().setDomStorageEnabled(true);
+    webView.getSettings().setSupportMultipleWindows(true);
+    webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+
+    CookieManager cookieManager = CookieManager.getInstance();
+    cookieManager.setAcceptCookie(true);
+    cookieManager.setAcceptThirdPartyCookies(webView, true);
+  }
+
+  private boolean createPopup(WebView opener, Message resultMsg) {
+    final WebView popup = new WebView(MainActivity.this);
+    popup.setBackgroundColor(Color.WHITE);
+    configureWebView(popup);
+
+    popup.setWebViewClient(new WebViewClient() {
+      @Override
+      public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+        return false;
+      }
+    });
+
+    popup.setWebChromeClient(new WebChromeClient() {
+      @Override
+      public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message childResultMsg) {
+        return createPopup(view, childResultMsg);
+      }
+
+      @Override
+      public void onCloseWindow(WebView window) {
+        closePopup(window);
+      }
+    });
+
+    FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+      FrameLayout.LayoutParams.MATCH_PARENT,
+      FrameLayout.LayoutParams.MATCH_PARENT
+    );
+    addContentView(popup, params);
+
+    WebView.WebViewTransport transport = (WebView.WebViewTransport) resultMsg.obj;
+    transport.setWebView(popup);
+    resultMsg.sendToTarget();
+    return true;
+  }
+
+  private void closePopup(WebView window) {
+    if (window == null || window == mainWebView) return;
+    ViewGroup parent = (ViewGroup) window.getParent();
+    if (parent != null) parent.removeView(window);
+    window.stopLoading();
+    window.destroy();
   }
 }
